4f5d342ab191c1e26ac9b0fa316c992644b199aa
Leonardo_Daitx <leonnardomelo3@gmail.com>
2023-10-03 08:54:01 -0300

    Continuando estudos

--- 5_OBJECT-TYPES/index.js
+++ 5_OBJECT-TYPES/index.js
@@ -4,3 +4,12 @@ console.log(car); // { brand: 'Chevrolet', wheels: 4 }
 const macacoPardo = { teste: 2, nome: 10, idade: 35 };
 macacoPardo.novo = 1;
 console.log(macacoPardo);
+// Ready-only array
+let myArr = [1, 2, 3]; // Criei uma array de números "readonly"
+const unidos = {
+    numeroUm: 1, numeroDois: "dois", numeroTres: 3, numeroQuatro: "quatro"
+};
+const unidosHeranca = {
+    numeroUmHeranca: 1, numeroDoisHeranca: "dois", numeroTresHeranca: 3, numeroQuatroHeranca: "quatro"
+};
+// console.log(unidosHeranca)
--- 5_OBJECT-TYPES/index.ts
+++ 5_OBJECT-TYPES/index.ts
@@ -17,3 +17,48 @@ interface signature {
 const macacoPardo: signature = {teste: 2, nome: 10, idade: 35}
 macacoPardo.novo = 1
 console.log(macacoPardo);
+
+// Ready-only array
+let myArr: ReadonlyArray<number> = [1, 2, 3] // Criei uma array de números "readonly"
+
+// myArr.push(20) // Impossível dar push por conta do "readonly"
+// myArr[3] = 13 // Impossível adicionar mais um elemento por conta do "readonly"
+// myArr[1] = 3  // Impossível modificar valores por conta do "readonly"
+
+// Somando interfaces
+interface interOne { // Crio a primeira interface
+  numeroUm: number
+  numeroDois: string
+}
+
+interface interTwo { // Crio a segunda interface
+  numeroTres: number
+  numeroQuatro: string
+}
+
+type unindoInterface = interOne | interTwo // Junto todas por meio do "type"
+
+const unidos: unindoInterface = { // Utilizo o meu "type" com as duas interfaces
+  numeroUm: 1, numeroDois: "dois", numeroTres: 3, numeroQuatro: "quatro"
+}
+
+// console.log(unidos)
+
+// Herança nas interfaces
+interface interOneHeranca { // Crio a interface pai
+  numeroUmHeranca: number
+  numeroDoisHeranca: string
+}
+
+interface interTwoHeranca extends interOneHeranca { // Crio a interface filho e junto as duas por meio do "extends"
+  numeroTresHeranca: number
+  numeroQuatroHeranca: string
+}
+
+
+
+const unidosHeranca: interTwoHeranca = { // Utilizo a interface filho pois terá as propriedades das duas interfaces
+  numeroUmHeranca: 1, numeroDoisHeranca: "dois", numeroTresHeranca: 3, numeroQuatroHeranca: "quatro"
+}
+
+// console.log(unidosHeranca)
